768746d7f9cdf8e8ea9fc601eb48c0d9e70788e8
Khalid.Rakhif <khalid.rakhif@gmail.com>
2024-09-22 15:31:21 -0400

    Squares of a Sorted Array

---
+++ src/com/leetcode/dataStructures/SortedSquares.java
@@ -0,0 +1,13 @@
+package com.leetcode.dataStructures;
+
+import java.util.Arrays;
+
+public class SortedSquares {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]= (int) Math.pow(nums[i],2);
+        }
+       Arrays.sort(nums);
+       return nums;
+    }
+}
