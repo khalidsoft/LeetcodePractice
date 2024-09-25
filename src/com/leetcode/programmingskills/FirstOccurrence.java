70b191bcda1561c6a97954ccf1b474250c95a058
Khalid.Rakhif <khalid.rakhif@gmail.com>
2024-09-24 20:49:06 -0400

    9/24/2024

--- src/com/leetcode/programmingskills/FirstOccurrence.java
+++ src/com/leetcode/programmingskills/FirstOccurrence.java
@@ -3,7 +3,7 @@ package com.leetcode.programmingskills;
 public class FirstOccurrence {
     public int strStr(String haystack, String needle) {
 
-        return haystack.indexOf(needle,0);
+        return haystack.indexOf(needle);
 
     }
 }
